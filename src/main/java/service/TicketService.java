1ead5d79644362cd8336dab925b87b74225403bf
Srijita Banerjee <srijitabanerjee55@gmail.com>
2020-11-17 23:03:38 +0530

    Reshmi

--- src/main/java/service/TicketService.java
+++ src/main/java/service/TicketService.java
@@ -1,14 +1,34 @@
 package service;
 
 import com.areteans.ticketbooking.config.PostgresManager;
-import models.Passengers;
-
+import models.Train;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.util.Map;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-//public class TicketService {
-    // Passengers createPassenger(Passengers Passengers) {
+public class TicketService {
+
+    public Train createTrain(Train train) {
+        jdbcTemplate.update("insert into train(start_city,end_city,train_no,fare) values (?,?,?,?)");
+
+    }
+
+        Connection connection = PostgresManager.getConnection();
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into train(name, contact, address, grade, section, percentage) values (?,?,?,?,?,?)");
+            preparedStatement.setString(1, start_city.getName());
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                return new train(resultSet.getString("name"), resultSet.getLong("studentID"), resultSet.getLong("contact"), null, 0, 'A', 0, null);
+            }
+    } catch(SQLException ex) {
+        System.err.println(ex.getMessage());
+    }
+        return null;
+            }
+
 
 
 
