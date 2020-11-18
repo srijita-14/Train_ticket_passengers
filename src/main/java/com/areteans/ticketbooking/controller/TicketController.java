c7fd1e488b7588765542cf8ee6bf79676e64dfcc
rajdeepd192 <rajdeepd192@gmail.com>
2020-11-18 09:42:09 +0530

    Rajdeep

--- src/main/java/com/areteans/ticketbooking/controller/TicketController.java
+++ src/main/java/com/areteans/ticketbooking/controller/TicketController.java
@@ -15,16 +15,15 @@ public class TicketController {
         return start_city != null && end_city != null ? start_city.toUpperCase() + ": " + end_city : "empty input";
     }
 
-    @GetMapping(path = "data/{train_no}")
-    public String getFromPV(@PathVariable("train_no") Integer train_no) {
-        return String.valueOf(train_no);
-    }
 
-    @PostMapping(path = "data", consumes = MediaType.APPLICATION_JSON_VALUE)
+
+    @PostMapping(path = "traindetails", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String getDetails(@RequestBody Map<String, String> input) {
         String start_city = input.get("start_city");
-        Integer train_no = Integer.parseInt(input.get("train_no"));
-        return start_city != null && train_no != null ? start_city.toUpperCase() + ": " + train_no : "empty input";
+        String end_city = input.get("end_city");
+        //Integer train_no = Integer.parseInt(input.get("train_no"));
+        Integer fare= Integer.parseInt(input.get("fare"));
+        return start_city != null  && end_city!= null && fare!= null? start_city.toUpperCase() + ": " + end_city.toUpperCase() + ": "+ fare.floatValue(): "empty input";
 
 
     }
